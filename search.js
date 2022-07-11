9e1f36930b322980d3eb390c3949afd958d244e0
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-07-11 19:33:52 +0300

    update again

--- search.js
+++ search.js
@@ -6,45 +6,25 @@ const searchWrapper = document.getElementById('search-wrapper')
 
 searchWrapper.innerHTML = `<h1>Search results:</h1>`
 fetch(`https://jsonplaceholder.typicode.com/users?username=${searchResult}`)
-.then(res => res.json())
-.then(results => {
-    results.map(result => {
-       
-        let searchName = document.createElement('h4')
-        searchName.innerHTML = `<a href="./user.html?user_id=${result.id}">${result.username}</a>`
+  .then(res => res.json())
+  .then(users => {
+    if (users.length > 0) {
+      users.map(user => {
+        let userItem = document.createElement('h4');
+        userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
 
-        searchWrapper.append(searchName)
-    })
-})
-
-fetch(`https://jsonplaceholder.typicode.com/users?name=${searchResult}`)
-.then(res => res.json())
-.then(results => {
-    results.map(result => {
-       
-        let searchName = document.createElement('h4')
-        searchName.innerHTML = `<a href="./user.html?user_id=${result.id}">${result.name}</a>`
-
-        searchWrapper.append(searchName)
-    })
-})
-
-fetch(`https://jsonplaceholder.typicode.com/albums?title=${searchResult}`)
-.then(res => res.json())
-.then(results => {
-    results.map(result => {
-        fetch(`https://jsonplaceholder.typicode.com/users?name=${searchResult}`)
+        searchWrapper.append(userItem);
+      })
+    } else {
+      fetch(`https://jsonplaceholder.typicode.com/users?name=${searchResult}`)
         .then(res => res.json())
-        .then(users => {
-            users.map(user => {
-                console.log(user)
-                let searchName = document.createElement('h4')
-                searchName.innerHTML = `<a href="./album.html?album_id=${result.id}&album_title=${result.title}&user_id=${result.userId}&user_name=${user.name}">${result.title}</a>`
-                searchWrapper.append(searchName)
-       
-       
-        
-    })
-})
-})
-})
+        .then(usersByName => {
+            usersByName.map(user => {
+                console.log(user.name)
+                let userItem = document.createElement('h4');
+                userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
+                searchWrapper.append(userItem);
+            })
+        })
+    }
+  })
