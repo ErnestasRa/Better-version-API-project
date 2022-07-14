acc3521f1ac9a568cb83f1fe50298b9dfa24c899
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-07-14 09:07:56 +0300

    fixed posts.js

--- posts.js
+++ posts.js
@@ -12,8 +12,8 @@ function renderAllPosts() {
     let urlParams = new URLSearchParams(queryParams);
     let userId = urlParams.get('user_id');
 
-    if(renderUserByIdPosts(userId)) {
-        
+    if(userId) {
+        renderUserByIdPosts(userId)
     } else {
         renderAllUserPosts()
     }
@@ -58,8 +58,7 @@ function renderUserByIdPosts(id){
     fetch(`https://jsonplaceholder.typicode.com/users/${id}?_embed=posts`)
     .then(res => res.json())
     .then(users => {
-        users.map(user => {
-            user.posts.map(post => {
+            users.posts.map(post => {
                         let updatedTitle = post.title[0].toUpperCase() + post.title.slice(1);
                         let postItem = document.createElement('div')
                         postItem.classList.add('post-item')
@@ -79,8 +78,8 @@ function renderUserByIdPosts(id){
                         let postCommentsEmail = document.createElement('h6')
                         postCommentsEmail.classList.add('post-comments-email')
 
-                        postBody.innerHTML = `<strong><a href="./post.html?post_id=${post.id}&post_title=${post.title}&user_name=${user.name}&post_body=${post.body}">${updatedTitle}</a></strong>`   
-                        postAuthor.innerHTML = `<a href="./user.html?user_id=${user.id}&user_name=${user.name}"> <strong>${user.name}</strong> </a>`
+                        postBody.innerHTML = `<strong><a href="./post.html?post_id=${post.id}&post_title=${post.title}&user_name=${users.name}&post_body=${post.body}">${updatedTitle}</a></strong>`   
+                        postAuthor.innerHTML = `<a href="./user.html?user_id=${users.id}&user_name=${users.name}"> <strong>${users.name}</strong> </a>`
                         
                         postItem.append(postAuthor,postBody,postCommentsTitle,postCommentsBody,postCommentsEmail)
                         postWrapper.append(postItem)
@@ -88,5 +87,5 @@ function renderUserByIdPosts(id){
             })
         })
        
-    })
-}
+    }
+
